3779e589d85764bd18d254a56d24fb4fbae30c80
andersonmacuche <andersonmacuche@gmail.com>
2023-08-22 18:15:55 +0200

    initial version

--- app/page.js
+++ app/page.js
@@ -1,95 +1,113 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import "./Homepage.css";
 
-export default function Home() {
+function Homepage() {
+  const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
+  const [signUpMode, setSignUpMode] = useState(false);
+
+  const toggleForm = () => {
+    setIsLoginFormVisible(!isLoginFormVisible);
+  };
+
+  useEffect(() => {
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
+
+    sign_up_btn.addEventListener("click", () => {
+      setSignUpMode(true);
+    });
+
+    sign_in_btn.addEventListener("click", () => {
+      setSignUpMode(false);
+    });
+
+    return () => {
+      // Clean up the event listeners
+      sign_up_btn.removeEventListener("click", () => {
+        setSignUpMode(true);
+      });
+      sign_in_btn.removeEventListener("click", () => {
+        setSignUpMode(false);
+      });
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={`container ${signUpMode ? "sign-up-mode" : ""}`}>
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="#" class="sign-in-form">
+            <h2 class="title">Entre</h2>
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Senha" />
+            </div>
+
+            <input type="submit" value="Entrar" class="btn solid" />
+          </form>
+          <form action="#" class="sign-up-form">
+            <h2 class="title">Inscreva-se</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Nome de usuario" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Senha" />
+            </div>
+            <div  class="input-field">
+            <i class="fas fa-lock"></i>
+
+              <input type="date" placeholder="Data de nascimento" />
+            </div>
+            <div  class="input-field">
+            <i class="fas fa-lock"></i>
+
+              <input placeholder="Telefone" />
+            </div>
+            <input type="submit" class="btn" value="Inscreva-se" />
+          </form>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>Novo aqui?</h3>
+            <p>
+              Inscreva-se e faca parte dos nossos cursos sit amet consectetur
+              adipisicing elit. Nostrum laboriosam ad deleniti.
+            </p>
+            <button class="btn transparent" id="sign-up-btn">
+              Inscreva-se
+            </button>
+          </div>
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>Ja es um de nos?</h3>
+            <p>
+              Entre e continue conosco sit amet consectetur adipisicing elit.
+              Nostrum laboriosam ad deleniti.
+            </p>
+            <button class="btn transparent" id="sign-in-btn">
+              Entrar
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default Homepage;
